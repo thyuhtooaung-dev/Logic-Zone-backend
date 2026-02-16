321f2ac3c18f54b281f5463817935a207c34e4d2
ThyuHtooAungOwO <thyuhtoo7@gmail.com>
2026-02-16 16:03:23 +0630

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express from "express";
+import cors from "cors";
+import subjectsRouter from "./routes/subjects"
 
 const app = express();
 const port = 3000
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 })
+
+app.use("/api/subjects", subjectsRouter);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
---
+++ src/routes/subjects.ts
@@ -0,0 +1,73 @@
+import express from "express";
+import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
+import { departments, subjects } from "../db/schema";
+import { db } from "../db";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const { search, department, page = 1, limit = 10 } = req.query;
+
+    const currentPage = Math.max(1, +page);
+    const limitPerPage = Math.max(1, +limit);
+
+    const offset = (currentPage - 1) * limitPerPage;
+
+    const filterCondition = [];
+
+    // if search query exists, filter by subject name OR subject code
+    if (search) {
+      filterCondition.push(
+        or(
+          ilike(subjects.name, `%${search}%`),
+          ilike(subjects.code, `%${search}%`),
+        ),
+      );
+    }
+
+    // if department filter exists, match department name
+    if (department) {
+      filterCondition.push(ilike(departments.name, `%${departments}%`));
+    }
+
+    // combine all filters using AND if any exist
+    const whereClause =
+      filterCondition.length > 0 ? and(...filterCondition) : undefined;
+
+    const countResult = await db
+      .select({ count: sql<number>`count(*)` })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause);
+
+    const totalCount = countResult[0]?.count ?? 0;
+
+    const subjectsList = await db
+      .select({
+        ...getTableColumns(subjects),
+        department: { ...getTableColumns(departments) },
+      })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause)
+      .orderBy(desc(subjects.createdAt))
+      .limit(limitPerPage)
+      .offset(offset);
+
+    res.status(200).json({
+      data: subjectsList,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        total: totalCount,
+        totalPage: Math.ceil(totalCount / limitPerPage),
+      }
+    })
+  } catch (err) {
+    console.log(`GET /subjects error: ${err}`);
+    res.status(500).json({ error: "Failed to get subjects" });
+  }
+});
+
+export default router;
